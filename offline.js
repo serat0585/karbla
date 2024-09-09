50b3c58d4c48430f479543fd98e871dbca149f9e
serat05857 <serat05857@gmail.com>
2024-09-10 01:15:18 +0330

    upload game new

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1725916306,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/jam-sheet0.png",
+		"images/qab-sheet0.png",
+		"images/rubikapngparspngcom2300x-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/ssss-sheet0.png",
+		"images/flyerdesigns-sheet0.png",
+		"images/wwdw-sheet0.png",
+		"images/sdkklk-sheet0.png",
+		"images/angosht-sheet0.png",
+		"images/لیستسایه-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"animate.min.css"
+	]
+}
